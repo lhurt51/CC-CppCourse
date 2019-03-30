7bbd60e76ff7ec1c6ce55156c8d714f7cd065cbe
Liam Hurt <liamhurt51@gmail.com>
2019-03-30 00:00:46 -0700

    Playing around with cmake and ctest

---
+++ SlotMachine/build/_CPack_Packages/Darwin-Source/TZ/slot_machine-0.1.1-Source/build/_CPack_Packages/Darwin-Source/TGZ/slot_machine-0.1.1-Source/build/GameConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+#define slot_machine_VERSION_MAJOR 0
+#define slot_machine_VERSION_MINOR 1
---
+++ SlotMachine/build/_CPack_Packages/Darwin-Source/TZ/slot_machine-0.1.1-Source/build/_CPack_Packages/Darwin-Source/TXZ/slot_machine-0.1.1-Source/build/_CPack_Packages/Darwin-Source/TGZ/slot_machine-0.1.1-Source/src/main.cpp
@@ -0,0 +1,24 @@
+
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "GameConfig.h"
+
+int main (int argc, char *argv[])
+{
+  if (argc < 2)
+    {
+    fprintf(stdout,"%s Version %d.%d\n",
+            argv[0],
+            slot_machine_VERSION_MAJOR,
+            slot_machine_VERSION_MINOR);
+    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    return 1;
+    }
+  double inputValue = atof(argv[1]);
+  double outputValue = sqrt(inputValue);
+  fprintf(stdout,"The square root of %g is %g\n",
+          inputValue, outputValue);
+  return 0;
+}
