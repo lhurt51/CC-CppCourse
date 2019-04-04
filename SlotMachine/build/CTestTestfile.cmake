ff5614917e585274360b86538154af8cedb5e18a
Liam Hurt <liamhurt51@gmail.com>
2019-04-03 20:37:10 -0700

    Deleting the build dirrectory

--- SlotMachine/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/liamhurt/code/CC-CppCourse/SlotMachine
-# Build directory: /Users/liamhurt/code/CC-CppCourse/SlotMachine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Runs "/Users/liamhurt/code/CC-CppCourse/SlotMachine/build/slot_machine")
-set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/Users/liamhurt/code/CC-CppCourse/SlotMachine/CMakeLists.txt;62;add_test;/Users/liamhurt/code/CC-CppCourse/SlotMachine/CMakeLists.txt;0;")
--- SlotMachine/build/_CPack_Packages/Darwin/STGZ/slot_machine-0.1.1-Darwin/include/GameConfig.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for Tutorial
-#define slot_machine_VERSION_MAJOR 0
-#define slot_machine_VERSION_MINOR 1
